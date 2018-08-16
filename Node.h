824c397f35d59d15733f7cdd217a134fb3e657e9
coleman <cwmchose@mix.wvu.edu>
2018-08-16 15:58:31 -0400

    Node header created

---
+++ Node.h
@@ -0,0 +1,23 @@
+#IFNDEF NODE_H
+#DEFINE NODE_H
+
+
+/* Structure */
+typedef struct Node
+{
+	Node* next;
+	void* data;
+}Node;
+
+
+/* Macros */
+#define TRUE  1
+#define FALSE 0 
+
+/* Prototypes */
+Node* createNode(void* data);
+int destroyNode(Node** node); 
+int setNext(Node* node, Node* next);
+int hasNext(Node* node);
+Node* getNext(Node* node);
+void* getData(Node* node);
